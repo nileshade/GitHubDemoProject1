48920e3232a23ebd287c1a8b7add935a0786b7e4
Nilesh Ravi Ade <nileshade75@gmail.com>
2022-07-14 21:30:30 +0530

    2nd commit Main.java file added

---
+++ src/com/fashion/Main.java
@@ -0,0 +1,12 @@
+package com.fashion;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is first GitHub Push");
+	}
+
+}
